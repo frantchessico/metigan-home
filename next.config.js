89e4cea452b4ecf5f7158f0ee2e0ec5b047725ee
frantchessico <contentscreatorsp@gmail.com>
2025-03-22 21:25:23 +0200

    remove eslint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com', 'res.cloudinary.com', 'images.pexels.com'],
       },
+      eslint: {
+        ignoreDuringBuilds: true,
+      }
 }
 
 module.exports = nextConfig
