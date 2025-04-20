f1532f982af32d875f0cfdaf3319ec05b2b84ea9
frantchessico <contentscreatorsp@gmail.com>
2025-04-20 23:36:04 +0200

    add x pixel

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -4,7 +4,7 @@ import Script from "next/script"
 import { useEffect } from "react"
 import { usePathname, useSearchParams } from "next/navigation"
 
-export default function GoogleAnalytics({ googleAdsId = "AW-17020158384" }) {
+export default function GoogleAnalytics({ googleAdsId = "AW-17020158384", twitterEventId = "tw-pkfiv-pkfix" }) {
   const pathname = usePathname()
   const searchParams = useSearchParams()
 
@@ -19,13 +19,69 @@ export default function GoogleAnalytics({ googleAdsId = "AW-17020158384" }) {
 
   return (
     <>
-      <Script src={`https://www.googletagmanager.com/gtag/js?id=AW-17020158384`} strategy="afterInteractive" />
+      {/* Google Analytics */}
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${googleAdsId}`} strategy="afterInteractive" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'AW-17020158384');
+          gtag('config', '${googleAdsId}');
+        `}
+      </Script>
+
+      {/* Twitter Pixel */}
+      <Script id="twitter-pixel" strategy="afterInteractive">
+        {`
+          !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+          },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+          a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+          twq('config','${twitterEventId}');
+        `}
+      </Script>
+
+      {/* Twitter Conversion Tracking */}
+      <Script id="twitter-conversion" strategy="afterInteractive">
+        {`
+          function setupTwitterConversion() {
+            if (typeof twq !== 'undefined') {
+              twq('event', '${twitterEventId}', {
+                value: null,
+                currency: null,
+                contents: [
+                  {
+                    content_type: null,
+                    content_id: null,
+                    content_name: null,
+                    content_price: null,
+                    num_items: null,
+                    content_group_id: null
+                  },
+                  {
+                    content_type: null,
+                    content_id: null,
+                    content_name: null,
+                    content_price: null,
+                    num_items: null,
+                    content_group_id: null
+                  }
+                ],
+                status: null,
+                conversion_id: null,
+                email_address: null,
+                phone_number: null
+              });
+            }
+          }
+          
+          // Run after Twitter pixel is loaded
+          if (typeof twq !== 'undefined') {
+            setupTwitterConversion();
+          } else {
+            window.addEventListener('load', function() {
+              setTimeout(setupTwitterConversion, 1000);
+            });
+          }
         `}
       </Script>
     </>
